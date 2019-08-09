e0b1a7cb929c52b2bd890e3304adf8315fab6d73
Robin Sierra <sierrar@student.ethz.ch>
2019-08-09 18:29:26 +0200

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,46 +1,53 @@
-#!/usr/bin/env python3
+"""
+Copyright (c) 2019 ETH Zurich
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
 
 from setuptools import setup, find_packages
 
 
 setup(
-        name='2vyper',
-        version='0.0.1',
-        author='Viper Team',
-        author_email='viper@inf.ethz.ch',
-        license='MPL-2.0',
-        packages=find_packages('src'),
-        package_dir={'': 'src'},
-        package_data={
-            'nagini_translation.resources': ['*.vpr'],
-            'nagini_translation.backends': ['*.jar']
-        },
-        requires=[
-            'distribute',
-            ],
-        install_requires=[
-            'jpype1==0.7.0',
-            'astunparse==1.6.2',
-            'z3-solver',
-            'vyper'
-            ],
-        entry_points={
-             'console_scripts': [
-                 '2vyper = nagini_translation.main:main',
-                 ]
-             },
-        url='http://www.pm.inf.ethz.ch/research/nagini.html',
-        description='Static verifier for Vyper, based on Viper.',
-        long_description=(open('README.rst').read()),
-        # Full list of classifiers could be found at:
-        # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Environment :: Console',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python :: 3 :: Only',
-            'Topic :: Software Development',
-            ],
-        )
+    name='2vyper',
+    version='0.0.1',
+    license='MPL-2.0',
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={
+        'nagini_translation.backends': ['*.jar'],
+        'nagini_translation.resources': ['*.vpr'],
+    },
+    install_requires=[
+        'astunparse==1.6.2',
+        'jpype1==0.7.0',
+        'vyper',
+        'z3-solver',
+    ],
+    tests_require=[
+        'pytest>=3.3.0'
+    ],
+    entry_points={
+        'console_scripts': [
+                '2vyper = nagini_translation.main:main',
+        ]
+    },
+
+    author='Viper Team',
+    author_email='viper@inf.ethz.ch',
+    url='http://www.pm.inf.ethz.ch/research/nagini.html',
+    description='Static verifier for Vyper, based on Viper.',
+    long_description=(open('README.rst').read()),
+    # Full list of classifiers could be found at:
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development',
+    ],
+)
