2d9085f70001ab1ddf3dcca986c96990725874cc
Robin Sierra <sierrar@student.ethz.ch>
2019-11-19 10:52:20 +0100

    Implemented check that a contract correctly implements ghost functions

--- src/twovyper/analysis/symbol_checker.py
+++ src/twovyper/analysis/symbol_checker.py
@@ -12,6 +12,7 @@ from twovyper.exceptions import InvalidProgramException
 
 def check_symbols(program: VyperProgram):
     _check_unique_ghost_functions(program)
+    _check_ghost_implements(program)
 
 
 def _check_unique_ghost_functions(program: VyperProgram):
@@ -19,3 +20,18 @@ def _check_unique_ghost_functions(program: VyperProgram):
         ghosts = sum(len(interface.ghost_functions) for interface in program.interfaces.values())
         if ghosts != len(program.ghost_functions):
             raise InvalidProgramException(program.node, 'duplicate.ghost')
+
+
+def _check_ghost_implements(program: VyperProgram):
+    def check(cond):
+        if not cond:
+            raise InvalidProgramException(implementation.node, 'ghost.not.implemented')
+
+    for itype in program.implements:
+        interface = program.interfaces[itype.name]
+        for ghost in interface.ghost_functions.values():
+            implementation = program.ghost_function_implementations.get(ghost.name)
+            check(implementation)
+            check(implementation.name == ghost.name)
+            check(len(implementation.args) == len(ghost.args))
+            check(implementation.type == ghost.type)
